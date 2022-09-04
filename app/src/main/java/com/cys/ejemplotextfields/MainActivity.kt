40b2c376044af83c78f3d180fdc1071a51dcdf70
sara <saracorraless01@gmail.com>
2022-09-04 18:45:26 +0200

    Checkbox

--- app/src/main/java/com/cys/ejemplotextfields/MainActivity.kt
+++ app/src/main/java/com/cys/ejemplotextfields/MainActivity.kt
@@ -2,10 +2,27 @@ package com.cys.ejemplotextfields
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var checkbox = findViewById<CheckBox>(R.id.checkBox)
+        var buttom = findViewById<Button>(R.id.button)
+        var textView = findViewById<TextView>(R.id.text)
+
+        buttom.setOnClickListener {
+            if (checkbox.isChecked){
+                Toast.makeText(this, "is checked", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "not checked", Toast.LENGTH_LONG).show()
+
+            }
+        }
     }
 }
